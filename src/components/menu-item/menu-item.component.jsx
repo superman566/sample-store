f9df1c2c07d343920bfb320c6c43a5585a3b95ad
superman566 <guochaohetony@gmail.com>
2021-07-07 01:55:58 -0400

    add withRouter into homepage

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,22 @@
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = props => (
-  <div
-    className={`${props.size} menu-item`}
-  >
+const MenuItem = (props) => {
+  return (
     <div
-      className={'background-image'}
-      style={{backgroundImage: `url(${props.imageUrl})`}}>
+      className={`${props.size} menu-item`}
+      onClick={()=> props.history.push(`${props.match.url}${props.linkUrl}`)}
+    >
+      <div
+        className={'background-image'}
+        style={{backgroundImage: `url(${props.imageUrl})`}}>
+      </div>
+      <div className={'content'}>
+        <h1 className={'title'}>{props.title.toUpperCase()}</h1>
+        <span className={'subtitle'}>Shop Now</span>
+      </div>
     </div>
-    <div className={'content'}>
-      <h1 className={'title'}>{props.title.toUpperCase()}</h1>
-      <span className={'subtitle'}>Shop Now</span>
-    </div>
-  </div>
-);
+  )
+};
 
-export default MenuItem;
+export default withRouter(MenuItem);
