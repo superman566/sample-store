84895e63ebbb29cb3f7837b49d2cd256b9bc1a29
superman566 <guochaohetony@gmail.com>
2021-07-18 03:17:03 -0400

    create user actions and reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from "redux";
+// import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-// const middlewares = [logger];
+const middlewares = [];
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middlewares),
+);
+
+const store = createStore(rootReducer, enhancer);
 
 export default store;
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,13 @@
+import { UserActionTypes }  from './user.types';
+
 const INITIAL_STATE = {
-  currentUser: null
+  currentUser: null,
+  age: 0
 };
 
 const userReducer = (state= INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
