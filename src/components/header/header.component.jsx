cb457e69fa6bf61431f82853d926bf0d852034bf
superman566 <guochaohetony@gmail.com>
2021-07-18 00:20:19 -0400

    add redux and reduxlogger

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -40,6 +41,10 @@ const Header = ({ currentUser}) => (
       }
     </div>
   </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
