8276cf382bf0e295b1c54f2f00be92adcd002ab8
superman566 <guochaohetony@gmail.com>
2021-07-25 02:06:22 -0400

    remove item from checkout

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,3 +9,15 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
   return [...cartItems, {...cartItemToAdd, quantity: 1}]
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const exsitingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+  if (exsitingCartItem.quantity === 1) {
+    return cartItems.filter( item=> item.id !== cartItemToRemove.id);
+  }
+  return cartItems.map( item =>
+    item.id === cartItemToRemove.id ?
+      {...item, quantity: item.quantity -1} :
+      item
+  )
+};
