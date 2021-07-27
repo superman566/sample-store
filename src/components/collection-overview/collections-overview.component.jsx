7f2a6c38003ada0e21256b7824da49567fc7c2cf
superman566 <guochaohetony@gmail.com>
2021-07-27 01:56:45 -0400

    data normalization

--- src/components/collection-overview/collections-overview.component.jsx
+++ src/components/collection-overview/collections-overview.component.jsx
@@ -4,20 +4,23 @@ import { createStructuredSelector } from "reselect";
 
 import './collections-overview.styles.scss';
 import CollectionPreview from "../collection-preview/collection-preview.component";
-import {selectCollections} from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
-const CollectionsOverview = ({ collections }) => (
-  <div className={'collections-overview'}>
-    {
-      collections.map( ({id, ...otherCollectionsProps}) => (
-        <CollectionPreview key={id} {...otherCollectionsProps} />
-      ))
-    }
-  </div>
-);
+const CollectionsOverview = ({collections}) => {
+  console.log(collections)
+  return (
+    <div className={'collections-overview'}>
+      {
+        collections.map( ({id, ...otherCollectionsProps}) => (
+          <CollectionPreview key={id} {...otherCollectionsProps} />
+        ))
+      }
+    </div>
+  )
+};
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import './collection.styles.scss';
+
 import {connect} from "react-redux";
 import {selectCollection} from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import './collection.styles.scss';
 
 const CollectionPage = ({collection}) => {
-  console.log('CategoryPage', collection);
+  const { title, items } = collection;
   return (
     <div className={'collection-page'}>
-      <h2>Category page</h2>
-
+      <h2 className={'title'}>{title}</h2>
+      <div className={'items'}>
+        {
+          items.map(item => (
+            <CollectionItem
+              key={item.id}
+              item={item}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 };
