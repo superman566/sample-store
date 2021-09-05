9ae2fe326edb27f89e369ba654d85cf72a6bc177
superman566 <guochaohetony@gmail.com>
2021-09-04 21:27:37 -0400

    hardcode title path in collection-preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,22 +1,25 @@
 import CollectionItem from '../collection-item/collection-item.component';
-import {CollectionPreviewContainer, PreviewContainer, Title} from "./collection-preview.styles";
+import {CollectionPreviewContainer, PreviewContainer, TitleLink} from "./collection-preview.styles";
 
-const CollectionPreview = ({title, items}) => (
-  <CollectionPreviewContainer>
-    <Title>{title}</Title>
-    <PreviewContainer>
-      {
-        items
-          .filter((item, id) => id < 4)
-          .map( item => (
-          <CollectionItem
-            key={item.id}
-            item={item}
-          />
-        ))
-      }
-    </PreviewContainer>
-  </CollectionPreviewContainer>
-)
+const CollectionPreview = ({title, items}) => {
+  const titlePath = title.toLowerCase();
+  return (
+    <CollectionPreviewContainer>
+      <TitleLink to={`shop/${titlePath}`}>{title}</TitleLink>
+      <PreviewContainer>
+        {
+          items
+            .filter((item, id) => id < 4)
+            .map( item => (
+              <CollectionItem
+                key={item.id}
+                item={item}
+              />
+            ))
+        }
+      </PreviewContainer>
+    </CollectionPreviewContainer>
+  )
+}
 
 export default CollectionPreview;
--- src/components/collection-preview/collection-preview.styles.jsx
+++ src/components/collection-preview/collection-preview.styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {Link} from "react-router-dom";
 
 export const CollectionPreviewContainer = styled.div`
   display: flex;
@@ -9,6 +10,12 @@ export const CollectionPreviewContainer = styled.div`
 export const Title = styled.h1`
   font-size: 28px;
   margin-bottom: 25px;
+`;
+
+export const TitleLink = styled(Link)`
+  font-size: 28px;
+  margin-bottom: 25px;
+  cursor: pointer;
 `;
 
 export const PreviewContainer = styled.div`
