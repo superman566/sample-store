cf1fa36dd6ddc8a36d524e3ead8e16ea3681d4c4
superman566 <guochaohetony@gmail.com>
2021-09-12 13:52:02 -0400

    add a backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -2,20 +2,35 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import {connect} from "react-redux";
 import {removeAllItemsFromCart} from "../../redux/cart/cart.actions";
+import axios from "axios";
+import {response} from "express";
 
 const StripeCheckoutButton = ({price, removeAll}) => {
   const priceForStripe = price * 100;
-  const publlishableKey = 'pk_test_51JIMwhK0H8OYdELC408hJulRTEVo7Oh1opAxjEfqAV9Y6PXcmQ8gApOqFqHTp7afKqc0sASIFq2W2I8KMSj8BtHB00EMVf8Jci';
+  const PublishableKey = 'pk_test_51JIMwhK0H8OYdELC408hJulRTEVo7Oh1opAxjEfqAV9Y6PXcmQ8gApOqFqHTp7afKqc0sASIFq2W2I8KMSj8BtHB00EMVf8Jci';
 
   const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment Successfully!');
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment!')
+    })
     removeAll();
     console.log(token);
-    alert('Payment Successfully!');
+
   };
 
   return (
     <StripeCheckout
-      stripeKey={publlishableKey}
+      stripeKey={PublishableKey}
       label={'Pay Now'}
       name={'Sample Store'}
       billingAddress
