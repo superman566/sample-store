3278f56bab595cc87ef48c950d747ec155e7f701
superman566 <guochaohetony@gmail.com>
2021-09-26 21:12:41 -0400

    apply pwa into our project

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
@@ -27,7 +28,7 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 // serviceWorkerRegistration.unregister();
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -17,6 +18,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 if(process.env.NODE_ENV ==='production') {
+  app.use(enforce.HTTPS({
+    trustProtoHeader: true
+  }));
+
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res){
@@ -28,6 +33,10 @@ app.listen(port, error => {
   if(error) throw error;
   console.log('Server running on port ' + port);
 })
+
+app.get('/service-worker.js', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 app.post('/payment', (req, res)=>{
   const body = {
